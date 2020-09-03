78d9687c4708690c1d1148441dfaf3cd3d40215f
jangky000 <jangky000@mme.dongguk.edu>
2020-09-04 02:27:34 +0900

    merge 충돌 해결

--- models/rooms.js
+++ models/rooms.js
@@ -8,22 +8,14 @@ J057_김태은  2:47 PM
     price는 할인가격, 이전가격, 가격이 스트링으로 넘어와서 정규식을 써줘야합니당..! ㅠ
 */
 
-<<<<<<< HEAD
 
 const rooms_json_arr = require('../rooms.json');
-=======
-// nedb 로드
-const Datastore = require('nedb');
-// 자동 로드 및 데이터베이스 파일 생성
-const db = new Datastore({filename: 'rooms.db', autoload: true});
->>>>>>> 16eb21b583094289d156ef1e3c8ec70c222bd605
 
 class Rooms{
     constructor(){
 
     }
 
-<<<<<<< HEAD
     // 일치 개수
     count(place, guests){
         return rooms_json_arr.reduce((pre, value) => {
@@ -40,67 +32,13 @@ class Rooms{
             if(value['place'] === place && maxGuests >= guests) pre.push(value);
             return pre;
         }, []);
-=======
-    // 삽입
-    create(json_arr){
-        return new Promise((resolve)=>{
-            db.insert(json_arr, function(err, newDoc){
-                // console.log('레코드 삽입 결과: ' + newDoc);
-                resolve(newDoc);
-            })
-        });
-    }
-
-    // 일치 개수
-    count(place, guests){
-        return new Promise((resolve)=>{
-            db.count({email:email}, function(err, count){
-                // console.log('테이블 레코드 전체 개수: ' + count);
-                resolve(count);
-            })
-        });
-    }
-
-    // 조건 검색
-    readByEmail(place, guests){
-        return new Promise((resolve)=>{
-            db.find({email: email}, function(err, docs){
-                // 레코드 검색 결과
-                // console.log(docs);
-                resolve(docs);
-            })
-        });
-    }
-
-    // 수정
-    update_rooms(place, title, description1, description2, price, image){
-        return new Promise((resolve)=>{
-            db.update({email:email}, {$set:{name: name, birth:birth}}, function(err, numDocs){
-                // console.log('레코드 수정 반영 수: ' + numDocs)
-                resolve(numDocs);
-            })
-        });
-    }
-
-    // 삭제
-    delete(email){
-        return new Promise((resolve)=>{
-            db.remove({email:email}, function(err, numDocs){
-                // console.log('레코드 삭제 반영 수: ' + numDocs)
-                resolve(numDocs);
-            })
-        });
->>>>>>> 16eb21b583094289d156ef1e3c8ec70c222bd605
     }
 
 }
 
 module.exports = Rooms
 
-<<<<<<< HEAD
 // const rooms = new Rooms();
 // console.log(rooms.count('서울', 5));
 // console.log(rooms.list('서울', 5));
 
-=======
->>>>>>> 16eb21b583094289d156ef1e3c8ec70c222bd605
