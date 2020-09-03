4bc407bab5c8c808a0f4a6b43b0e0cdf85742699
jangky000 <jangky000@mme.dongguk.edu>
2020-09-04 02:26:38 +0900

    merge 충돌 해결

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD
 const Rooms = require('../models/rooms')
 
 const rooms = new Rooms();
@@ -11,13 +10,6 @@ router.get('/rooms', function(req, res){
     const list = rooms.list(req.query.location, req.query.guests);
     const count = list.length;
     res.render('search/rooms', {location:req.query.location, checkin: req.query.checkin, checkout:req.query.checkout, guests:req.query.guests, count: count, list: list});
-=======
-// 방 검색 결과
-router.get('/rooms', function(req, res){
-    // console.log("방 검색");
-    // console.log(req.query);
-    res.render('search/rooms', {location: req.query.location, checkin: req.query.checkin, checkout:req.query.checkout, guests:req.query.guests});
->>>>>>> 16eb21b583094289d156ef1e3c8ec70c222bd605
 });
 
 module.exports = router;
