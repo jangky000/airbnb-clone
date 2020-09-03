b108e430c80d9462f0146a8e6920bb7111b33942
jangky000 <jangky000@mme.dongguk.edu>
2020-09-03 17:21:52 +0900

    회원 메뉴 모달 추가

--- models/rooms.js
+++ models/rooms.js
@@ -1,10 +1,11 @@
 /*
 rooms.json 파일 출처
 J057_김태은  2:47 PM
-실제 airbnb 사이트에서 더미데이터를 크롤링해보았습니다.
-속성은
-place(“서울” | “제주” | “경주” |“전주” | “속초“)
-title description1, description2, price, image를 가지고있고, 데이터 개수는 place당 60개, 총 300개입니다. 
+    실제 airbnb 사이트에서 더미데이터를 크롤링해보았습니다.
+    속성은
+    place(“서울” | “제주” | “경주” |“전주” | “속초“)
+    title description1, description2, price, image를 가지고있고, 데이터 개수는 place당 60개, 총 300개입니다.
+    price는 할인가격, 이전가격, 가격이 스트링으로 넘어와서 정규식을 써줘야합니당..! ㅠ
 */
 
 class rooms{
