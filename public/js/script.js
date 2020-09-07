e42ecaa1a25b9ffad5585ed692c7c7b8cadb6c0c
jangky000 <jangky000@mme.dongguk.edu>
2020-09-07 23:03:01 +0900

    why? 회원가입 모달 레이아웃 추가
    how? modal 기법 사용 및 에어비앤비 사이트 디자인 참고

--- public/js/script.js
+++ public/js/script.js
@@ -9,23 +9,25 @@ document.querySelector("body").addEventListener('click', close_user_nav_menu);
 // header tab + search
 function toggle_search_tab(){
     document.getElementById('search_rooms').classList.toggle('hidden');
-    document.getElementById('search_rooms').classList.toggle('show');
+    // document.getElementById('search_rooms').classList.toggle('show');
     document.getElementById('search_experiences').classList.toggle('hidden');
-    document.getElementById('search_experiences').classList.toggle('show');
+    // document.getElementById('search_experiences').classList.toggle('show');
 }
 
 // user_nav_modal 
 function toggle_user_nav_menu(){
-    document.getElementById('user_nav_menu').classList.toggle('show');
+    // document.getElementById('user_nav_menu').classList.toggle('show');
     document.getElementById('user_nav_menu').classList.toggle('hidden');
 }
 
+// 외부 영역을 클릭하면 메뉴바 닫기
 function close_user_nav_menu(e){
     const menu = document.getElementById('user_nav_menu');
     const btn = document.getElementById('btn_user_nav');
-    if(!(menu.contains(e.target) || btn.contains(e.target))){
-        document.getElementById('user_nav_menu').classList.toggle('show');
-        document.getElementById('user_nav_menu').classList.toggle('hidden');
+    // alert(e.target);
+    if(!(menu.contains(e.target) || btn.contains(e.target))&&!menu.classList.contains('hidden')){
+        // document.getElementById('user_nav_menu').classList.toggle('show');
+        menu.classList.toggle('hidden');
     }
 }
 
