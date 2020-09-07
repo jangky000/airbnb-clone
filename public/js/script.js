318f9d8586b1b020cc9900d24fa20ac2e79e827d
jangky000 <jangky000@mme.dongguk.edu>
2020-09-07 23:35:20 +0900

    why? 회원가입 모달 기능 연결
    how? JS 이벤트 연결

--- public/js/script.js
+++ public/js/script.js
@@ -5,18 +5,19 @@ document.getElementById('tab_rooms').addEventListener('click', toggle_search_tab
 document.getElementById('tab_experiences').addEventListener('click', toggle_search_tab);
 document.getElementById('btn_user_nav').addEventListener('click', toggle_user_nav_menu);
 document.querySelector("body").addEventListener('click', close_user_nav_menu);
+document.getElementById("open_register").addEventListener('click', show_register_modal);
+document.querySelector(".close").addEventListener('click', close_modal);
+document.querySelector(".modal_overlay").addEventListener('click', close_modal);
+
 
 // header tab + search
 function toggle_search_tab(){
     document.getElementById('search_rooms').classList.toggle('hidden');
-    // document.getElementById('search_rooms').classList.toggle('show');
     document.getElementById('search_experiences').classList.toggle('hidden');
-    // document.getElementById('search_experiences').classList.toggle('show');
 }
 
 // user_nav_modal 
 function toggle_user_nav_menu(){
-    // document.getElementById('user_nav_menu').classList.toggle('show');
     document.getElementById('user_nav_menu').classList.toggle('hidden');
 }
 
@@ -24,10 +25,20 @@ function toggle_user_nav_menu(){
 function close_user_nav_menu(e){
     const menu = document.getElementById('user_nav_menu');
     const btn = document.getElementById('btn_user_nav');
-    // alert(e.target);
     if(!(menu.contains(e.target) || btn.contains(e.target))&&!menu.classList.contains('hidden')){
-        // document.getElementById('user_nav_menu').classList.toggle('show');
         menu.classList.toggle('hidden');
     }
 }
 
+function show_register_modal(){
+    toggle_user_nav_menu();
+    const modal = document.getElementById("modal_register");
+    modal.classList.toggle('hidden');
+}
+
+
+function close_modal(e){
+    // const modal = e.target.parentNode;
+    const modal = document.querySelector(".modal");
+    modal.classList.toggle('hidden');
+}
